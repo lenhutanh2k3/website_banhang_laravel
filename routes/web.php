b27c88bcfda5bc88071a2efadf482b2e45b5b4d5
Le Nhut Anh <nhutanhle130403@gmail.com>
2025-02-11 11:49:44 +0700

    Slide layout home

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
-
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[HomeController::class,'index']);
+Route::resource('home',HomeController::class);
